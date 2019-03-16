1543cd04bb6cfd60a8aad0efe3cc210532eca46c
Alejandro Olmedo <00097017@uca.edu.sv>
2019-03-16 11:31:32 -0600

    Actualizado metodo para escoger la imagen aleatoria

--- app/src/main/java/com/example/olmedo/galeria/MainActivity.java
+++ app/src/main/java/com/example/olmedo/galeria/MainActivity.java
@@ -28,6 +28,8 @@ final class MainActivity extends AppCompatActivity implements View.OnClickListen
         img8 = findViewById(R.id.img8);
         img9 = findViewById(R.id.img9);
 
+
+
         img1.setOnClickListener(this);
         img2.setOnClickListener(this);
         img3.setOnClickListener(this);
@@ -41,15 +43,14 @@ final class MainActivity extends AppCompatActivity implements View.OnClickListen
 
     }
 
+
     @Override
     public void onClick(View v) {
-        ImageView[] imagenes = {img1, img2, img3, img4, img5, img6, img7, img8, img9};
 
-        ImageView imagenNueva  = imagenes[new Random().nextInt(imagenes.length)];
-        Drawable nueva = imagenNueva.getDrawable();
-
+        int imagenes[] = {R.drawable.img01, R.drawable.img02, R.drawable.img03, R.drawable.img04, R.drawable.img05, R.drawable.img06, R.drawable.img07, R.drawable.img08, R.drawable.img09};
+        int posicionArrayNuevaImagen  = new Random().nextInt(imagenes.length);
         int idCelda = v.getId();
         ImageView celda = findViewById(idCelda);
-        celda.setImageDrawable(nueva);
+        celda.setImageResource(imagenes[posicionArrayNuevaImagen]);
     }
 }
