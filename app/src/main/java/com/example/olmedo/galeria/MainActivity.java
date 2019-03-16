9ce0376a93e20705ea80e84c4676d2b4ab4cc8e1
Alejandro Olmedo <00097017@uca.edu.sv>
2019-03-16 11:06:36 -0600

    Subiendo ejercicio 2

---
+++ app/src/main/java/com/example/olmedo/galeria/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.olmedo.galeria;
+
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.Random;
+
+final class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        img3 = findViewById(R.id.img3);
+        img4 = findViewById(R.id.img4);
+        img5 = findViewById(R.id.img5);
+        img6 = findViewById(R.id.img6);
+        img7 = findViewById(R.id.img7);
+        img8 = findViewById(R.id.img8);
+        img9 = findViewById(R.id.img9);
+
+        img1.setOnClickListener(this);
+        img2.setOnClickListener(this);
+        img3.setOnClickListener(this);
+        img4.setOnClickListener(this);
+        img5.setOnClickListener(this);
+        img6.setOnClickListener(this);
+        img7.setOnClickListener(this);
+        img8.setOnClickListener(this);
+        img9.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        ImageView[] imagenes = {img1, img2, img3, img4, img5, img6, img7, img8, img9};
+
+        ImageView imagenNueva  = imagenes[new Random().nextInt(imagenes.length)];
+        Drawable nueva = imagenNueva.getDrawable();
+
+        int idCelda = v.getId();
+        ImageView celda = findViewById(idCelda);
+        celda.setImageDrawable(nueva);
+    }
+}
